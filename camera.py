5fec3868ff7d78eea96922e7b8cced2555040893
Thanh Giang <giang.phan@alta.com.vn>
2023-08-29 20:07:54 +0700

    fix: page

--- camera.py
+++ camera.py
@@ -26,8 +26,7 @@ if os.path.exists(_camera_env):
     with open(_camera_env, 'rt') as file:
         size = file.readline()
         info = size.split(",")
-        CAMERAS = (int(info[0]), int(info[1]), int(
-            info[2]), int(info[3]), int(info[4]))
+        CAMERAS = (int(info[0]), max(int(info[1]),10), max(int(info[2]),10), int(info[3]), int(info[4]))
 
 elif os.path.exists(_camera_env_old):
     with open(_camera_env_old, 'rt') as file:
